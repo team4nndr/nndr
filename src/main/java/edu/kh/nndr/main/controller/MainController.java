b5fcc6a8784dcc265ef7c7bce2bc291c23628cb7
yuna <cyunanne@gmail.com>
2023-05-08 21:58:55 +0900

    MainController 테스트 코드 삭제

--- src/main/java/edu/kh/nndr/main/controller/MainController.java
+++ src/main/java/edu/kh/nndr/main/controller/MainController.java
@@ -1,23 +1,14 @@
 package edu.kh.nndr.main.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import edu.kh.nndr.member.model.dto.Member;
-import edu.kh.nndr.member.model.service.MemberService;
-
 @Controller
 public class MainController {
 	
-	@Autowired
-	MemberService service;
-	
 	@RequestMapping("/")
 	public String mainForward(Model model) {
-		Member testMember = service.test();
-		model.addAttribute("testMember", testMember); // request scope
 		return "main/main";
 	}
 }
