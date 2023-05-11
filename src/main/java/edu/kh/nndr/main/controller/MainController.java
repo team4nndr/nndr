cf18a1547b904a1b16648058ae807513b6090dce
pakjongwook <meso8587@naver.com>
2023-05-11 21:21:00 +0900

    비밀번호를 잊으셨습니까?

--- src/main/java/edu/kh/nndr/main/controller/MainController.java
+++ src/main/java/edu/kh/nndr/main/controller/MainController.java
@@ -2,13 +2,21 @@ package edu.kh.nndr.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
-	@RequestMapping("/")
-	public String mainForward(Model model) {
-		return "main/main";
+
+	@RequestMapping("/") // 실제로 보는 주소창
+	public String mainForward() {
+		return "main/main.jsp"; // 폴더 안에 있는 / 내가 보여줄 jsp 경로
 	}
+	
+	
+	@GetMapping("/main/forgetPw")
+	public String forgetPw() {
+		return "main/forgetPw";
+	}
+	
 }
