98daccb4fac12455835982d18787da0c115dd0b5
pakjongwook <meso8587@naver.com>
2023-05-12 10:53:58 +0900

    비밀번호를잊으셨습니까 수정

--- src/main/java/edu/kh/nndr/main/controller/MainController.java
+++ src/main/java/edu/kh/nndr/main/controller/MainController.java
@@ -1,7 +1,6 @@
 package edu.kh.nndr.main.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +9,7 @@ public class MainController {
 
 	@RequestMapping("/") // 실제로 보는 주소창
 	public String mainForward() {
-		return "main/main.jsp"; // 폴더 안에 있는 / 내가 보여줄 jsp 경로
+		return "main/main"; // 폴더 안에 있는 / 내가 보여줄 jsp 경로
 	}
 	
 	
